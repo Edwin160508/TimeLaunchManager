b7ec6c56c4e3aefa6a1187d01c560c440767d21c
Edwin Lima <edwin.limaa@gmail.com>
2018-10-18 15:19:49 -0300

    EntityClass Classe que implementa interface serializable, essa classe serar usadada para heranca das classes do tipo entidade de banco.

---
+++ src/main/java/com/edinnov/api/entities/EntityClass.java
@@ -0,0 +1,10 @@
+package com.edinnov.api.entities;
+
+import java.io.Serializable;
+
+public class EntityClass implements Serializable {
+
+	
+	private static final long serialVersionUID = 3125148707838168105L;
+
+}
